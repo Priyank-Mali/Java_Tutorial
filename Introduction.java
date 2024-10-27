b4c6ab510b16754c015daa5679aed76052b5f784
🇵‌🇷‌🇮‌🇾‌🇦‌🇳‌🇰‌ 🇲‌🇦‌🇱‌🇮‌ <priyankmali297@gmail.com>
2024-10-27 17:25:02 +0530

    java

--- Introduction.java
+++ Introduction.java
@@ -1,3 +1,7 @@
 public class Introduction {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello World !!");
+    }
 }
+
+// https://www.w3schools.com/java/java_intro.asp
